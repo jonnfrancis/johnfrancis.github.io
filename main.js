78f51c03b44964fa955ffe0df4ae91c5454398b9
John Francis <johnmosima7@gmail.com>
2024-01-02 05:25:07 +0300

    fixed assets render

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
     import './style.css'
     import './font.css'
 
+    console.log("Portfolio is live!")
+
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
         apiKey: import.meta.env.DBNAME,
