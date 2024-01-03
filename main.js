dbd1ca6431e3de638d070e9be2ac954e409e0b92
John Francis <johnmosima7@gmail.com>
2024-01-03 07:08:24 +0300

    fixed lenis

--- main.js
+++ main.js
@@ -18,17 +18,7 @@
 
     gsap.registerPlugin(ScrollTrigger);
 
-    const lenis = new Lenis({
-        duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-    })
-
-    function raf(time) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
+    
 
 
     addHeroSection()
